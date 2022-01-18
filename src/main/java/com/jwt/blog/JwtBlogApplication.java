c9d950072d3dfd1a957de8999a33c138c142a304
mukund-singh <kumarbalmukund4u@gmail.com>
2022-01-18 10:57:24 +0530

    Spring Boot JWT Blog

---
+++ src/main/java/com/jwt/blog/JwtBlogApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwt/blog/JwtBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
